5aa1fd694290a0bb98076420086948a1edfa3689
AnggaNaa <angga.ardiansyah056@gmail.com>
2023-11-10 07:01:11 +0700

    fix: CRUD

--- pages/detail/[id].vue
+++ pages/detail/[id].vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <template>
-      <div class="container mx-auto p-8">
+      <div class="container mx-auto mt-24 p-8">
         <div class="max-w-2xl mx-auto">
           <img
+            v-if="item"
             :src="item.image"
             alt="Image"
             class="w-full h-64 object-cover rounded-md mb-6"
           />
 
-          <h1 class="text-3xl font-bold mb-4">{{ item.title }}</h1>
+          <h1 class="text-3xl font-bold mb-4" v-if="item">{{ item.title }}</h1>
           <div
+            v-if="item"
             v-html="item.detail"
             class="text-base text-gray-700 leading-relaxed mb-8"
           ></div>
@@ -24,6 +26,28 @@ import dataDummy from "../utils/dataDummy.json";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+const item = ref(null);
 
-const item = dataDummy.find((item) => item.id == route.params.id);
+const fetchData = async () => {
+  const itemId = route.params.id;
+  const result = dataDummy.find((item) => item.id == itemId);
+
+  if (result) {
+    item.value = result;
+  } else {
+    // Handle case when item is not found
+    console.error("Item not found");
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
+
+// import dataDummy from "../utils/dataDummy.json";
+// import { useRoute } from "vue-router";
+
+// const route = useRoute();
+
+// const item = dataDummy.find((item) => item.id == route.params.id);
 </script>
